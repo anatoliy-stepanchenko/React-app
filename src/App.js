1b9ea76ce56155caa46b6049a72e6fc861d9f7d3
anatoliy-stepanchenko <stepanchenko1991@gmail.com>
2021-10-27 15:45:14 +0300

    added createPost, removePost, sortPost

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import PostList from "./components/PostList";
+import PostForm from "./components/UI/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import './styles/App.css'
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript', body: 'Description'},
+    {id: 2, title: 'JavaScript 2', body: 'Description'},
+    {id: 3, title: 'JavaScript 3', body: 'Description'},
+  ]);
+
+  const [selectedSort, setSelectedSort] = useState('');
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id));
+  }
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostForm create={createPost}/>
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue='Сортировка'
+          options={[
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'}
+          ]}
+        />
+      </div>
+      {posts.length !== 0
+        ? <PostList remove={removePost} posts={posts} title={'Посты про JS'}/>
+        : <h1 style={{textAlign: 'center'}}>Посты не найдены</h1>
+      }
     </div>
   );
 }
