a7e883bd020982f0055d0465b3809a6d8d6071f6
anatoliy-stepanchenko <stepanchenko1991@gmail.com>
2021-11-01 15:22:08 +0200

    animation PostList(used React Transition Group)

--- src/components/UI/PostList.jsx
+++ src/components/UI/PostList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { TransitionGroup } from 'react-transition-group';
 import PostItem from './PostItem';
+import { CSSTransition } from 'react-transition-group';
 
 const PostList = ({posts, title, remove}) => {
 
@@ -12,8 +14,17 @@ const PostList = ({posts, title, remove}) => {
     return (
         <div>
             <h1 style={{textAlign: 'center'}}>{title}</h1>
-            {posts.map((post, index) => 
-            <PostItem remove ={remove} number={index + 1} post={post} key={post.id}/> )}
+            <TransitionGroup>
+                {posts.map((post, index) => 
+                <CSSTransition
+                    key={post.id}
+                    timeout={500}
+                    classNames="post"
+                >
+                    <PostItem remove ={remove} number={index + 1} post={post} />
+                </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 };
